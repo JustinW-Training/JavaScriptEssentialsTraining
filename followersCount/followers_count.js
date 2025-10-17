e0812d6be3d674b92e951e69c4830e109909f2c2
JustinW-Training <jdw-training@hotmail.com>
2025-10-17 15:24:23 -0400

    message

---
+++ followersCount/followers_count.js
@@ -0,0 +1,19 @@
+let count = 0; // Initialize count to 0
+
+function increaseCount() {
+  count++; // Verhoog de count met 1
+  displayCount(); // Toon de count
+  checkCountValue(); // Controleer de countwaarde en toon berichten
+}
+
+function displayCount() {
+document.getElementById('countDisplay').innerHTML=count; // Toon het aantal in de HTML
+}
+
+function checkCountValue() {
+  if (count === 10) {
+    alert("Je Instagram-post heeft 10 volgers gewonnen! Gefeliciteerd!");
+  } else if (count === 20) {
+    alert("Je Instagram-post heeft 20 volgers gewonnen! Ga zo door!");
+  }
+}
